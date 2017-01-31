5dc9baf8ca3778e2e8adda5f570bd0eedc628491
Ascold <ascold@ex.ua>
2017-01-31 16:31:03 +0200

    5 levels without stop

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 // first level
-while (north(isFree)) {
-    north();
-    console.log(map());
+function firstLevel(){
+    while (north(isFree)) {
+        north(isFree);
+        console.log(map());
+        if (north(isFree) == false)
+            return
+    }
 }
+firstLevel();
+
 
 // second level
-while (east(isFree)) {
-    east();
+while (true) {
+    east(isFree);
     console.log(map());
+    if (east(isFree) == false)
+        break
+
 }
 
-// third and fourth
+// third
 while (south(isFree) || east(isFree)) {
     if (south(isFree)) {
         south();
@@ -19,4 +28,53 @@ while (south(isFree) || east(isFree)) {
         east();
         console.log(map());
     }
+}
+
+// fourth
+while (south(isFree) || east(isFree)) {
+    if (east(isFree)) {
+        east();
+        console.log(map());
+    } else if (south(isFree)) {
+        south();
+        console.log(map());
+    }
+}
+
+// fifth
+for (; east(isFree);) {
+    east();
+    console.log(map());
+}
+for (; south(isFree);) {
+    south();
+    console.log(map());
+}
+for (; west(isFree);) {
+    west();
+    console.log(map());
+}
+for (; south(isFree);) {
+    south();
+    console.log(map());
+}
+for (; west(isFree);) {
+    west();
+    console.log(map());
+}
+for (; north(isFree);) {
+    north();
+    console.log(map());
+}
+for (; west(isFree);) {
+    west();
+    console.log(map());
+}
+for (; south(isFree);) {
+    south();
+    console.log(map());
+}
+for (; east(isFree);) {
+    east();
+    console.log(map());
 }
