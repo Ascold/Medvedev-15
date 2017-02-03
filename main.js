cdd3160ca98446f2973a877bbb077b0dd7dac8d5
Ascold <ascold@ex.ua>
2017-02-04 01:00:52 +0200

    stop is working

--- main.js
+++ main.js
@@ -1,73 +1,40 @@
-// first level
-var firstLevel = function () {
-    while (isFree('north')) {
-        north();
-        console.log(map());
+var myFunction = function () {
+    var stepResult = "";
+    while (true) {
+        if (isFree('south')) {
+            while (isFree('south')) {
+                stepResult = south();
+            }
+            if (stepResult == "next") {
+                break
+            }
+        }
+        else if (isFree('north')) {
+            while (isFree('north')) {
+                stepResult = north();
+            }
+            if (stepResult == "next") {
+                break
+            }
+        }
+        if (isFree('east')) {
+            while (isFree('east')) {
+                stepResult = east();
+            }
+            if (stepResult == "next") {
+                break
+            }
+        }
+        else if (isFree('west')) {
+            while (isFree('west')) {
+                stepResult = west();
+            }
+            if (stepResult == "next") {
+                break
+            }
+        }
     }
 };
-firstLevel();
-
-
-// second level
-var secondLevel = function () {
-    while (isFree('east')) {
-        east(isFree);
-        console.log(map());
-    }
-};
-secondLevel();
-
-// third
-var thirdLevel = function () {
-    while (isFree('east') || isFree('south')) {
-        if (isFree('east') == false && isFree('south') == false)
-            break;
-        east();
-        console.log(map());
-        south();
-        console.log(map());
-    }
-};
-thirdLevel();
-
-// fourth
-while (south(isFree) || east(isFree)) {
-    if (east(isFree)) {
-        east();
-        console.log(map());
-    }
-    if (south(isFree)) {
-        south();
-        console.log(map());
-    }
-}
-
-// fifth
-while (isFree('east')) {
-    east(isFree);
-    console.log(map());
-}
-while (true) {
-    if (isFree('south')) {
-        while (isFree('south'))
-            south();
-        console.log(map());
-    }
-    else if (isFree('north')) {
-        while (isFree('north'))
-            north();
-        console.log(map());
-    }
-    if (isFree('east')) {
-        while (isFree('east'))
-            east();
-        console.log(map());
-    }
-    else if (isFree('west')) {
-        while (isFree('west'))
-            west();
-        console.log(map());
-    }
-}
+myFunction();
 
 
