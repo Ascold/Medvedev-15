cbef0ad6cdaac343093605098fb5406f6d78282d
Ascold <ascold@ex.ua>
2017-02-04 18:21:27 +0200

    add continous loop breaker

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-var myFunction = function () {
+function myFunction() {
+    var maxStepCount = 70;
     var stepResult = "";
-    while (true) {
+    for (var i= 0; i <= maxStepCount; i++) {
         if (isFree('south')) {
             while (isFree('south')) {
                 stepResult = south();
             }
-            if (stepResult == "next") {
-                break
-            }
-        }
-        else if (isFree('north')) {
+        } else if (isFree('north')) {
             while (isFree('north')) {
                 stepResult = north();
             }
-            if (stepResult == "next") {
-                break
-            }
         }
+        if (stepResult == "next") break;
+
         if (isFree('east')) {
             while (isFree('east')) {
                 stepResult = east();
             }
-            if (stepResult == "next") {
-                break
-            }
-        }
-        else if (isFree('west')) {
+        } else if (isFree('west')) {
             while (isFree('west')) {
                 stepResult = west();
             }
-            if (stepResult == "next") {
-                break
-            }
+        }
+        if (stepResult == "next") break;
+
+        if (i == maxStepCount){
+            console.log( "YOU SHALL NOT PASS!!!")
         }
     }
-};
-myFunction();
+}
 
-
+for (var i = 1; i < 6; i++) {
+    myFunction();
+    i == 5 ? console.log("5 Levels completed") : console.log("Level " + i + " passed")
+}
